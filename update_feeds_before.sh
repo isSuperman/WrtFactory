5f9b61e1b337cbe28bd8b472e15ad510cfec4f17
iSuperman <sup.github@outlook.com>
2021-06-16 09:02:42 +0800

    Update update_feeds_before.sh

--- update_feeds_before.sh
+++ update_feeds_before.sh
@@ -11,7 +11,10 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add helloworld
+echo "src-git helloworld https://github.com/immortalwrt/lean-lede/tree/helloworld" >> feeds.conf.default
