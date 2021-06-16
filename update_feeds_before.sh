07c8a34c79058422902aaa601d68c40d392b1531
iSuperman <sup.github@outlook.com>
2021-06-16 10:05:29 +0800

    Update update_feeds_before.sh

--- update_feeds_before.sh
+++ update_feeds_before.sh
@@ -10,11 +10,5 @@
 # Description: Wrt DIY script (Before Update feeds)
 #
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 # Add helloworld
 echo "src-git helloworld src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
