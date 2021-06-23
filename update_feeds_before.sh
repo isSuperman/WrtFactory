ffb354ac2152bbd92b5e15d32e59b461aea546ab
iSuperman <isSuperman@users.noreply.github.com>
2021-06-23 15:44:56 +0800

    fix

--- update_feeds_before.sh
+++ update_feeds_before.sh
@@ -11,7 +11,7 @@
 #
 
 # Add custom feeds
-sed -i ‘1,$d’ feeds.conf.default
+rf -f feeds.conf.default && touch feeds.conf.default
 sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
 sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
